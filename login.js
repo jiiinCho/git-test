93ff5305ab7e5235321456654453874da58c5769
jiiin <jiiin.cho.dev@gmail.com>
2022-04-19 14:56:31 +0200

    Login updated

--- login.js
+++ login.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const login = (props) => <h1>Login</h1>;
+const login = (props) => (
+  <>
+    <h1>Login</h1>
+    <p>Paragraph</p>
+  </>
+);
 
 export default login;
