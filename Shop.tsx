dbd4b5058b630215e05b68d94f905517d559ce54
jiiin <jiiin.cho.dev@gmail.com>
2022-04-19 15:01:16 +0200

    Shop updated

--- Shop.tsx
+++ Shop.tsx
@@ -1,5 +1,10 @@
 // import React from "react";
 
-const Shop = (props) => <h1>Shop</h1>;
+const Shop = (props) => (
+  <>
+    <h1>Shop</h1>
+    <h3>Shop grid</h3>
+  </>
+);
 
 export default Shop;
