c5812be406bc4bdbc5dd39222ab212b0ddc07b50
jiiin <jiiin.cho.dev@gmail.com>
2022-04-25 11:13:25 +0200

    Product description included

--- Shop.tsx
+++ Shop.tsx
@@ -5,6 +5,12 @@ const Shop = (props) => (
     <h1>Shop</h1>
     <h3 className="shop-grid">Shop grid</h3>
     <img src="src" alt="product" />
+    <h3>Product description</h3>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed aspernatur
+      numquam, dolor nobis iusto, harum doloribus quae quas aperiam voluptatibus
+      eum esse eos praesentium inventore. Libero quod ea iste odit?
+    </p>
   </>
 );
 
